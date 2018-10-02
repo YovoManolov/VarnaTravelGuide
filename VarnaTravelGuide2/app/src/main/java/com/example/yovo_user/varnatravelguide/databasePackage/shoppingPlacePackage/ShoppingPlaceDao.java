f41b894de85c34b4e8f779d6672957e09eee1a7b
YovoManolov <yovo.manolov@abv.bg>
2018-10-03 00:39:30 +0300

    03.10.2018 update
    03.10.2018 update

--- VarnaTravelGuide2/app/src/main/java/com/example/yovo_user/varnatravelguide/databasePackage/shoppingPlacePackage/ShoppingPlaceDao.java
+++ VarnaTravelGuide2/app/src/main/java/com/example/yovo_user/varnatravelguide/databasePackage/shoppingPlacePackage/ShoppingPlaceDao.java
@@ -7,10 +7,13 @@ import com.example.yovo_user.varnatravelguide.databasePackage.imagePackage.Image
 import java.util.List;
 
 public interface ShoppingPlaceDao {
+
     public void createShoppingPlacesTable(SQLiteDatabase dbWritableConnection);
     public void addShoppingPlaces(SQLiteDatabase dbWritableConnection,
                          ShoppingPlace[] shoppingPlaces);
-    public List<ShoppingPlace> getAllShoppingPlaces(SQLiteDatabase dbReadableConnection);
+    public List<ShoppingPlace> getAllShoppingPlaces(
+            SQLiteDatabase dbReadableConnection);
     public ShoppingPlace getShoppingPlaceByPlaceId
             (SQLiteDatabase dbWritableConnection,Integer placeId);
+
 }
